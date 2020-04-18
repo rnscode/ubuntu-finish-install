3685e5269a5286d875729b42d08f5abe56ef59e2
Robson Santos <36481078+rnscode@users.noreply.github.com>
2020-04-18 17:45:17 -0300

    commit v1

---
+++ ubuntu-finish-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt install vim -y
+sudo apt install curl -y
+sudo apt install tilix -y
+sudo apt install php-cli -y
